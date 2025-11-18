1f5d491b137ffb3f2540eeccdf62103c271418ea
Elijah Peiffer <elijah.peiffer@student.swic.edu>
2025-11-17 20:10:52 -0600

    Implement random student selection functionality in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import "./style.css";
+import data from "./db";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
+const button = document.querySelector("button");
+const nameDisplay = document.querySelector("p");
+
+button.addEventListener("click", () => {
+  console.log(getRandomIntInclusive(0, data.length - 1));
+});
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
