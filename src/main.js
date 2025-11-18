a54504da3eaa5a0da5c389779e2dbf276e95b5a3
Elijah Peiffer <elijah.peiffer@student.swic.edu>
2025-11-17 20:35:32 -0600

    Add theme selection functionality with dark and light mode options

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import "./style.css";
 import data from "./db";
 
+const body = document.querySelector("body");
 const button = document.querySelector("button");
 const nameDisplay = document.querySelector("p");
+const darkModeRadio = document.getElementById("dark");
+const lightModeRadio = document.getElementById("light");
 
 button.addEventListener("click", () => {
   nameDisplay.textContent =
     data[getRandomIntInclusive(0, data.length - 1)].name;
+});
+
+darkModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-yellow-500");
+  body.classList.add("bg-gray-800", "text-white");
+  console.log("Dark mode selected");
+});
+
+lightModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-800", "text-white");
+  body.classList.add("bg-yellow-500");
+  console.log("Light mode selected");
 });
 
 function getRandomIntInclusive(min, max) {
