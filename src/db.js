43892485239126462da93baabe0501802089b087
Elijah Peiffer <elijah.peiffer@student.swic.edu>
2025-11-17 19:46:44 -0600

    Add initial student data to the database

---
+++ src/db.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "Elijah Peiffer",
+  },
+  {
+    id: 2,
+    name: "Katelyn Kiehn",
+  },
+  {
+    id: 3,
+    name: "Jazmyn Koss",
+  },
+  {
+    id: 4,
+    name: "Kaitlyn O'Konner",
+  },
+  {
+    id: 5,
+    name: "Cecilia Wiza",
+  },
+];
